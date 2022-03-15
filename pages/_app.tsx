66c84c20080835c07a6c7447d5b4e788f2be4d1d
Iliyan Kupenov <i.kupenov96@gmail.com>
2022-03-15 18:12:33 +0200

    add redux toolkit; add ts lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import '../styles/globals.scss'
 
 import { theme } from '../styles'
+import { store } from '../app/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   )
 }
 
