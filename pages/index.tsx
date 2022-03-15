3eb0a80b494b89a50c6e332c23c78969abfdc1ce
Iliyan Kupenov <i.kupenov96@gmail.com>
2022-03-15 18:24:01 +0200

    adding redux boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,37 @@
 import type { NextPage } from 'next'
 
-import { Box } from '@chakra-ui/react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Text
+} from '@chakra-ui/react'
 
-const Home: NextPage = () => (
-  <Box>
-    Hello
-  </Box>
-)
+import { RootState } from '../app/store'
+import { decrement, increment } from '../features/counter'
+
+const Home: NextPage = () => {
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <Box>
+      Hello
+
+      <Text>{count}</Text>
+
+      <ButtonGroup>
+        <Button onClick={() => dispatch(increment())}>
+          Increment
+        </Button>
+
+        <Button onClick={() => dispatch(decrement())}>
+          Decrement
+        </Button>
+      </ButtonGroup>
+    </Box>
+  )
+}
 
 export default Home
