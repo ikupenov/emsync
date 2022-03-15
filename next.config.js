9a7f7b6641eced36a8175f7bf17698e7114d7f68
Iliyan Kupenov <i.kupenov96@gmail.com>
2022-03-15 17:51:51 +0200

    linting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT
+  },
+  images: {
+    domains: []
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 
 import { Box } from '@chakra-ui/react'
 
-const Home: NextPage = () => {
-  return (
-    <Box bgColor="primary.500">
-      Hello
-    </Box>
-  )
-}
+const Home: NextPage = () => (
+  <Box>
+    Hello
+  </Box>
+)
 
 export default Home
- 
