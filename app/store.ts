484c2058fa95fd05f3e209a2857c5fadb33b149e
Iliyan Kupenov <i.kupenov96@gmail.com>
2022-03-17 01:04:36 +0200

    adding blacklist persist

--- app/store.ts
+++ app/store.ts
@@ -37,7 +37,7 @@ const hydratedReducer = (
 }
 
 const persistedReducer = persistReducer(
-  { key: 'emsync', storage },
+  { key: 'emsync', storage, blacklist: [spotifyApi.reducerPath] },
   hydratedReducer as Reducer<ReturnType<typeof combinedReducer>>
 )
 
